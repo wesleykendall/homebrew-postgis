136a2a13660c437df79414642f177465af66eaa0
Wes Kendall <wesleykendall@gmail.com>
2017-10-27 10:25:24 -0700

    Update postgis@9.6.rb

--- postgis@9.6.rb
+++ postgis@9.6.rb
@@ -1,5 +1,5 @@
-class Postgis < Formula
-  desc "Adds support for geographic objects to PostgreSQL"
+class PostgisAT96 < Formula
+  desc "Adds support for geographic objects to PostgreSQL 9.6"
   homepage "https://postgis.net/"
   url "http://download.osgeo.org/postgis/source/postgis-2.4.0.tar.gz"
   sha256 "02baa90f04da41e04b6c18eedfda53110c45ae943d4e65050f6d202f7de07d29"
