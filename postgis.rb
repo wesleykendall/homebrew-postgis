a67e0ba35d6044d5eab40062cf6742dec766c427
Wes Kendall <wesleykendall@gmail.com>
2017-10-27 12:33:40 -0700

    Update postgis.rb

--- postgis.rb
+++ postgis.rb
@@ -26,7 +26,7 @@ class Postgis < Formula
 
   depends_on "pkg-config" => :build
   depends_on "gpp" => :build
-  depends_on "postgresql93"
+  depends_on "postgresql@9.6"
   depends_on "proj"
   depends_on "geos"
 
